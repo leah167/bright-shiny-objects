6b07d7f6b7b92545209188e0b776e001500f511b
Leah Sanchez <leahsanchez@Leahs-MacBook-Pro.local>
2022-02-24 15:35:24 -0700

    added functions for testing

--- main.js
+++ main.js
@@ -2,6 +2,93 @@
 // │ Your code here! │
 // └─────────────────┘
 
+function createUser(firstName, lastName) {
+    let fullName = {
+        firstName: firstName,
+        lastName: lastName
+        // or firstName,
+        // lastName since the object names are the same
+    }
+    return fullName;
+}
+
+// console.log(createUser('Mezi', 'Kaleziq'));
+
+
+
+function setAge(user, age){
+    user.age = age
+    return user
+};
+
+
+function incrementAge(user) {
+    
+    user.age = user.age + 1
+    return user
+
+}
+
+function fixCar(car) {
+    car.needsMaintenance = false
+    return car;
+    
+}
+
+function addGrades(student, newGrades) {
+    student.grades = student.grades.concat(newGrades);
+    return student
+    
+}
+
+function getDataType(object, key) {
+
+    return typeof object[key];
+    
+}
+
+function addTodo(todos, newTodo) {
+
+    todos.push(newTodo)
+    return todos;
+    
+}
+
+function addSong(playlist, newSong) {
+    playlist.duration += newSong.duration
+    playlist.songs.push(newSong)
+    return playlist
+    
+}
+
+
+function updateReportCard(reportCard, newGrade) {
+
+    reportCard.grades.push(newGrade)
+    reportCard.lowestGrade = Math.min(...reportCard.grades);
+    reportCard.highestGrade = Math.max(...reportCard.grades);
+
+    let sum = 0
+    
+    // for (let i = 0; i < reportCard.grades.length; i++) {
+    //     sum += reportCard.grades[i];
+
+    // }
+
+    // or
+
+    for (let grade of reportCard.grades) {
+        sum += grade;
+        
+    }
+
+    reportCard.averageGrade = sum / reportCard.grades.length;
+
+    return reportCard;
+
+    
+}
+
 
 
 
